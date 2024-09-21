2f311f1f734e7fe5f85da0566a632f97a6cd4fa7
NoRayze <pierremarco34270@gmail.com>
2024-09-21 23:32:51 +0200

    taille

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1100,  // Par exemple, augmente la limite à 1000 kB
+  },
+
 })
