2e4aa1867c9b3ebb84befa8cdb82f92c62e30617
NoRayze <pierremarco34270@gmail.com>
2024-09-22 12:24:00 +0200

    backgroundImage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/background_hero.webp')",
+        "hero-pattern": "url('/src/assets/herobg.jpg')",
       },
     },
   },
